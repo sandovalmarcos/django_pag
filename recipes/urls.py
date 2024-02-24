042c62f11322bc02ec77d7e31a2c3b00b4225116
Marcos Sandoval <sandovalhmarcos@gmail.com>
2024-02-24 19:14:18 -0300

    asda

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from recipes.views import home
+from . import views
 
 urlpatterns = [
-    path("", home, name="home"),
+    path("", views.home, name="home"),
+    path("recipes/<int:id>", views.recipe),
 ]
